9051933fe053a637ee7cb61d3414088a06c0d512
Gareth Boucher <garboucher@gmail.com>
2021-05-02 19:41:17 +0100

    Working autoloader

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require '../app/bootstrap.php';
+
+echo 'This is the root index file';
+
+class RootController extends App\common\Controller
+{
+    // TODO
+}
+
+?>
---
+++ app/common/Controller.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\common;
+class Controller 
+{
+   //TODO
+}
+?>
