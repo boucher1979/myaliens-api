e38dcd1e33fd0e6caeb1180b1b6e6dda9452bb1a
Gareth Boucher <garboucher@gmail.com>
2021-05-03 14:05:51 +0100

    Logging added using Monolog

--- api/index.php
+++ api/index.php
@@ -6,7 +6,13 @@ echo 'This is the root index file';
 
 class RootController extends App\common\Controller
 {
+    function __construct()
+    {
+        parent::__construct();
+        $this->log->info('Root controller instantiated');
+    }   
     // TODO
 }
 
+$controller = new RootController();
 ?>
--- app/common/Controller.php
+++ app/common/Controller.php
@@ -1,7 +1,23 @@
 <?php
 namespace App\common;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 class Controller 
 {
-   //TODO
+    protected $log;
+
+    function __construct()
+    {
+        $this->InitiateLogging();
+    }
+
+    private function InitiateLogging()
+    {
+        $this->log = new Logger('controller');
+        $this->log->pushHandler(new StreamHandler('D:/myaliens/logs/api.log', Logger::WARNING));
+        $this->log->pushHandler(new StreamHandler('D:/myaliens/logs/api.log', Logger::INFO));
+    }
 }
 ?>
